7045bdcf6379838a06c5e3cdf01711f19d5d695e
Koko Ono <nyswzt.a@gmail.com>
2025-02-13 18:02:46 +0900

    feat: add game limit and show result

--- src/app/game.js
+++ src/app/game.js
@@ -7,12 +7,15 @@ import {
   faHandPaper,
   faFlag,
 } from "@fortawesome/free-solid-svg-icons";
+import { createPortal } from "react-dom";
 
 export default function Game() {
   const [compScore, setCompScore] = useState([]);
   const [playerScore, setPlayerScore] = useState([]);
   const [playerValue, setPlayerValue] = useState(null);
   const [compValue, setCompValue] = useState(null);
+  const [gameCount, setGameCount] = useState(0);
+  const [winner, setWinner] = useState(null);
 
   const pointFlag = (key, color) => {
     return (
@@ -41,6 +44,12 @@ export default function Game() {
   };
 
   const decision = (playerChoice) => {
+    if (gameCount >= 5) {
+      showResult();
+      return;
+    }
+    setGameCount(gameCount + 1);
+    console.log(`I am from game Count${gameCount}`);
     const choices = ["ROCK", "PAPER", "SCISSORS"];
     const compChoice = choices[Math.floor(Math.random() * choices.length)];
     const val = logic(playerChoice, compChoice);
@@ -59,11 +68,31 @@ export default function Game() {
       setCompValue(compChoice);
       setPlayerValue(playerChoice);
     }
+
+    if (gameCount + 1 === 5) {
+      setTimeout(() => showResult(), 500); // Delay to allow UI updates
+    }
+  };
+
+  const showResult = () => {
+    const playerResult = playerScore.length;
+    const compResult = compScore.length;
+    var resultMessage =
+      playerResult > compResult
+        ? "You won the game!!"
+        : playerResult < compResult
+        ? "You lost the game!"
+        : "It's a tie!!";
+    alert(resultMessage);
+    resetResult();
   };
 
   const resetResult = () => {
     setPlayerScore([]);
     setCompScore([]);
+    setPlayerValue(null);
+    setCompValue(null);
+    setGameCount(0);
   };
 
   return (
@@ -85,6 +114,7 @@ export default function Game() {
         <p>Computer's choice: {compValue}</p>
         <h2>Your Score: {playerScore}</h2>
         <h2>Computer Score: {compScore}</h2>
+
         <button onClick={() => resetResult()}>Reset</button>
       </div>
     </div>
